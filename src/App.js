1ddf0987dc370392761910ba5359fb2ec9d6e0e3
CamiloMunozAL <camilo.munoz2@utp.edu.co>
2024-06-25 04:06:50 -0500

    Se agrego primer componente

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Testimonio from "./componentes/Testimonio.js";
 
+// Componente principal de la aplicación
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="contenedor-principal">
+        <h1>Testimonios: esto es lo que dicen los pibes</h1>
+        <Testimonio
+          imagen="emma"
+          nombre="Emma Bostian"
+          pais="Suecia"
+          cargo="Ingeniera de minas y lapiceros"
+          empresa="Chocolates S.A."
+          testimonio="Me gusta crear muchas cosas, pero sobre todo me gusta el chocolate. ¡Gracias por todo!"
+        />
+      </div>
     </div>
   );
 }
